5600b9bb3efd25628a76f95f435f8d6dc4a5ee41
Stephon Brown <Stephonb14@gmail.com>
2021-06-24 19:18:16 -0400

    updates to connection

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ namespace ToDoList
                     webBuilder.ConfigureAppConfiguration(config =>
                     {
                         var settings = config.Build();
-                        var connection = settings["ConnectionStrings:TestDB"];
+                        var connection = settings.GetConnectionString("AppConfig");
                         config.AddAzureAppConfiguration(connection);
                     }).UseStartup<Startup>();
                 });
